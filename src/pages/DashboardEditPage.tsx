0ded6858055a83306dde0ec1df308403a2d8bf7e
Valerio Como <v.como@innovazione.gov.it>
2025-03-11 17:05:51 +0000

    refactoring: added zustand

--- src/pages/DashboardEditPage.tsx
+++ src/pages/DashboardEditPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
 import { Link, useParams } from "react-router-dom";
 import useSWR from "swr";
+import { create } from "zustand";
 import Layout from "../components/layout";
 import Dialog from "../components/layout/Dialog";
 import Loading from "../components/layout/Loading";
@@ -23,6 +24,96 @@ interface ChartSelectionProps {
   charts: Record<string, TChart>;
   onSelect: (chart: TChart) => void;
 }
+
+interface DashboardEditState {
+  breakpoint: string;
+  layout: TLayoutItem[];
+  show: boolean;
+  lastCreated?: string;
+  selectedChart?: TChart;
+  updatedLayout: TLayoutItem[];
+  charts: Record<string, TChart>;
+  setBreakpoint: (breakpoint: string) => void;
+  setLayout: (layout: TLayoutItem[]) => void;
+  setShow: (show: boolean) => void;
+  setLastCreated: (lastCreated?: string) => void;
+  setSelectedChart: (selectedChart?: TChart) => void;
+  setUpdatedLayout: (updatedLayout: TLayoutItem[]) => void;
+  setCharts: (charts: Record<string, TChart>) => void;
+  addItem: () => void;
+  deleteItem: (id: string) => void;
+  showAddModal: (i: string) => void;
+  closeAddModal: () => void;
+  onDataChange: (data: {
+    charts: Record<string, TChart>;
+    layout: Array<TLayoutItem>;
+  }) => void;
+}
+
+const useDashboardEditStore = create<DashboardEditState>((set, get) => ({
+  breakpoint: "lg",
+  layout: [],
+  show: false,
+  lastCreated: undefined,
+  selectedChart: undefined,
+  updatedLayout: [],
+  charts: {},
+
+  setBreakpoint: (breakpoint) => set({ breakpoint }),
+  setLayout: (layout) => set({ layout }),
+  setShow: (show) => set({ show }),
+  setLastCreated: (lastCreated) => set({ lastCreated }),
+  setSelectedChart: (selectedChart) => set({ selectedChart }),
+  setUpdatedLayout: (updatedLayout) => set({ updatedLayout }),
+  setCharts: (charts) => set({ charts }),
+
+  addItem: () => {
+    const { layout } = get();
+    const xMax = layout.reduce((acc, cur) => (cur.x > acc ? cur.x : acc), 0);
+    const yMax = layout.reduce((acc, cur) => (cur.y > acc ? cur.y : acc), 0);
+    const count = layout.length ?? 0;
+    const i = `item-${count}`;
+    const l = { i, x: xMax, y: yMax, w: 1, h: 1 };
+    const newLayout = [...layout, l] as typeof layout;
+
+    set({
+      show: true,
+      lastCreated: i,
+      layout: newLayout,
+      updatedLayout: layout,
+    });
+  },
+  deleteItem: (id: string) => {
+    console.log("delete", id);
+    const { layout, updatedLayout } = get();
+    set({
+      layout: layout.filter((i) => i.i !== id),
+      updatedLayout: updatedLayout.filter((i) => i.i !== id),
+    });
+  },
+  onDataChange: ({
+    charts,
+    layout,
+  }: {
+    charts: Record<string, TChart>;
+    layout: Array<TLayoutItem>;
+  }) => {
+    set({ charts, layout });
+  },
+  showAddModal: (i: string) => {
+    set({ show: true, lastCreated: i });
+  },
+  closeAddModal: () => {
+    const { charts, lastCreated, selectedChart } = get();
+
+    set({
+      charts: { ...charts, [lastCreated as string]: selectedChart },
+      show: false,
+      lastCreated: undefined,
+      selectedChart: undefined,
+    });
+  },
+}));
 
 function ChartSelection(props: ChartSelectionProps) {
   const [charts, setCharts] = React.useState<Array<TChart>>([]);
@@ -96,71 +187,55 @@ function DashboardEditPage() {
     revalidateOnFocus: false,
   });
 
-  const [breakpoint, setBreakpoint] = React.useState("lg");
-  const [layout, setLayout] = React.useState<Array<TLayoutItem>>([]);
-  const [show, setShow] = React.useState(false);
-  const [lastCreated, setLastCreated] = React.useState<string>();
-  const [selectedChart, setSelectedChart] = React.useState<TChart>();
-  const [updatedLayout, setUpdatedLayout] = React.useState<Array<TLayoutItem>>(
-    []
-  );
-
-  const [charts, setCharts] = React.useState<Record<string, TChart>>({});
-
-  function showAddModal(i: string) {
-    setShow(true);
-    setLastCreated(i);
-  }
+  const {
+    breakpoint,
+    layout,
+    show,
+    lastCreated,
+    selectedChart,
+    updatedLayout,
+    charts,
+    setBreakpoint,
+    setLayout,
+    setShow,
+    setLastCreated,
+    setSelectedChart,
+    setUpdatedLayout,
+    setCharts,
+    addItem,
+    deleteItem,
+    showAddModal,
+    closeAddModal,
+    onDataChange,
+  } = useDashboardEditStore();
 
   function addChart(item: string) {
     console.log("addChart");
     showAddModal(item);
   }
 
-  function addItem() {
-    const xMax = layout.reduce((acc, cur) => (cur.x > acc ? cur.x : acc), 0);
-    const yMax = layout.reduce((acc, cur) => (cur.y > acc ? cur.y : acc), 0);
-    const count = layout.length ?? 0;
-    const i = `item-${count}` as `item-${number}`;
-    const l = {
-      i,
-      x: xMax,
-      y: yMax,
-      w: 1,
-      h: 1,
-    };
-    const newLayout = [...layout, l];
-    setLayout(newLayout);
-    setUpdatedLayout(newLayout);
-    showAddModal(i);
-  }
-  function deleteItem(id: string) {
-    console.log("delete", id);
-    setLayout((l) => l.filter((i) => i.i !== id));
-    setUpdatedLayout((l) => l.filter((i) => i.i !== id));
+  function addItemHandler() {
+    addItem();
   }
 
-  async function save() {
+  function deleteItemHandler(id: string) {
+    deleteItem(id);
+  }
+
+  async function saveHandler() {
     const body = {
       slots: updatedLayout.map((l) => ({
-        chartId: charts[l.i].id,
-        settings: {
-          i: l.i,
-          w: l.w,
-          h: l.h,
-          x: l.x,
-          y: l.y,
-        },
+        chartId: charts[l.i]?.id,
+        settings: { i: l.i, w: l.w, h: l.h, x: l.x, y: l.y },
       })),
     };
     const response = await updateSlots(id!, body);
-
     if (response) {
       reload();
     }
   }
 
-  function reset() {
+  function resetHandler() {
     reload();
   }
 
@@ -173,148 +248,100 @@ function DashboardEditPage() {
     console.log("effect", data);
     if (data) {
       const layout = data.slots.map(
-        (s: { settings: TLayoutItem; chart: TChart }) => {
-          return {
-            ...s.settings,
-            chart: s.chart,
-          };
-        }
+        ({ settings, chart }: { settings: TLayoutItem; chart: TChart }) => ({
+          ...settings,
+          chart,
+        })
       );
-      const charts = layout.reduce((p: any, c: { i: any; chart: any }) => {
-        return { ...p, [c.i]: c.chart };
-      }, {});
-      setCharts(charts);
-      setLayout(layout);
+      const charts = layout.reduce(
+        (p: any, c: { i: any; chart: any }) => ({ ...p, [c.i]: c.chart }),
+        {}
+      );
+
+      onDataChange({ charts, layout });
     }
-  }, [data]);
+  }, [data, setCharts, setLayout]);
 
   return (
     <Layout>
       <div className="flex justify-between items-center">
-        <Link to={"/dashboards"} className="text-blue-500 hover:underline">
+        <Link to="/dashboards" className="text-blue-500 hover:underline">
           Torna alla lista
         </Link>
         <div className="ml-auto flex space-x-2">
-          <button onClick={() => reset()} className="btn btn-primary">
+          <button onClick={resetHandler} className="btn btn-primary">
             Reset
           </button>
-          <button onClick={() => save()} className="btn btn-primary">
+          <button onClick={saveHandler} className="btn btn-primary">
             Salva
           </button>
         </div>
       </div>
-      <div className="">
-        {isLoading && <Loading />}
-        {error && (
-          <div role="alert" className="alert alert-error">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 shrink-0 stroke-current"
-              fill="none"
-              viewBox="0 0 24 24"
+      {isLoading && <Loading />}
+      {error && (
+        <div role="alert" className="alert alert-error">
+          {error.message}
+        </div>
+      )}
+      {data && (
+        <>
+          <h1 className="text-4xl font-bold">{data.name}</h1>
+          <h4 className="text-xl">{data.description}</h4>
+          <div className="flex flex-wrap items-center">
+            <button
+              className="m-2 btn btn-xs btn-primary"
+              onClick={addItemHandler}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>{error.message}</span>
-          </div>
-        )}
-        {data && (
-          <div>
-            <h1 className="text-4xl font-bold">{data.name}</h1>
-            <h4 className="text-xl">{data.description}</h4>
-            <div className="flex flex-wrap items-center">
+              Add +
+            </button>
+            {layout.map((l) => (
               <button
-                type="button"
-                className="m-2 btn btn-xs btn-primary"
-                onClick={() => addItem()}
+                key={l.i}
+                className="m-2 btn btn-xs btn-error"
+                onClick={() => deleteItemHandler(l.i)}
               >
-                Add +
+                {l.i}
               </button>
-
-              {layout
-                .sort((a, b) => {
-                  if (a.y === b.y) {
-                    return a.x - b.x;
-                  }
-                  return a.y - b.y;
-                })
-                .map((l) => (
-                  <button
-                    key={"delete" + l.i}
-                    className="m-2 btn btn-xs btn-error"
-                    onClick={() => deleteItem(l.i)}
-                  >
-                    {l.i}
-                  </button>
-                ))}
-            </div>
-            <div className="relative border min-h-[60vh]">
-              <ResponsiveReactGridLayout
-                onLayoutChange={(l: any) => {
-                  console.log("layout change", l);
-                  setUpdatedLayout(l);
-                }}
-                onBreakpointChange={(breakpoint, columns) => {
-                  console.log("breakpoint", breakpoint);
-                  console.log("columns", columns);
-                  setBreakpoint(breakpoint);
-                }}
-                className="react-grid-layout"
-                layouts={{
-                  lg: layout,
-                }}
-                cols={cols}
-                margin={[10, 10]}
-                rowHeight={360}
-              >
-                {layout.map((item) => (
-                  <div className="react-grid-item overflow-hidden" key={item.i}>
-                    <p>{item.i}</p>
-                    {charts && charts[item.i] ? (
-                      <RenderChart {...(charts[item.i] as any)} fullH={360} />
-                    ) : (
-                      <button
-                        type="button"
-                        className="m-2 btn btn-xs btn-primary"
-                        onClick={() => addChart(item.i)}
-                      >
-                        Add Chart +
-                      </button>
-                    )}
-                  </div>
-                ))}
-              </ResponsiveReactGridLayout>
-            </div>
-            {/* <div className='overflow-y-scroll h-[250px]'>
-              <small>
-                <pre>{layout && JSON.stringify(layout, null, 2)}</pre>
-              </small>
-            </div> */}
+            ))}
           </div>
-        )}
-      </div>
+          <div className="relative border min-h-[60vh]">
+            <ResponsiveReactGridLayout
+              onLayoutChange={setUpdatedLayout}
+              onBreakpointChange={setBreakpoint}
+              className="react-grid-layout"
+              layouts={{ lg: layout }}
+              cols={cols}
+              margin={[10, 10]}
+              rowHeight={360}
+            >
+              {layout.map((item) => (
+                <div className="react-grid-item overflow-hidden" key={item.i}>
+                  <p>{item.i}</p>
+                  {charts[item.i] ? (
+                    <RenderChart {...charts[item.i]} fullH={360} />
+                  ) : (
+                    <button
+                      className="m-2 btn btn-xs btn-primary"
+                      onClick={() => addChart(item.i)}
+                    >
+                      Add Chart +
+                    </button>
+                  )}
+                </div>
+              ))}
+            </ResponsiveReactGridLayout>
+          </div>
+        </>
+      )}
       {show && (
         <Dialog
           toggle={show}
           title="Select a chart"
           callback={() => {
-            setCharts({ ...charts, [lastCreated!]: { ...selectedChart } });
-            setLastCreated(undefined);
-            setSelectedChart(undefined);
-            setShow(false);
+            closeAddModal();
           }}
         >
-          <ChartSelection
-            charts={charts}
-            onSelect={(item) => {
-              setSelectedChart(item);
-            }}
-          />
+          <ChartSelection charts={charts} onSelect={setSelectedChart} />
         </Dialog>
       )}
     </Layout>
