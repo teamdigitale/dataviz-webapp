05e4e245eab1aa1c197e223d62b2554ee1dee63b
Lorezz <point.point@gmail.com>
2025-03-26 13:13:30 +0100

    chore: set classheight

--- src/pages/DashboardViewPage.tsx
+++ src/pages/DashboardViewPage.tsx
@@ -8,7 +8,8 @@ import RenderChart from "../components/RenderCellChart";
 import useDashboardViewStore from "../store/dashboard-view.store";
 
 const ROW_HEIGHT = 360;
-const WIDGET_HEIGHT = 48;
+const WIDGET_HEIGHT = 60;
+const ch = `min-h-[${WIDGET_HEIGHT}px]`;
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 const cols = { lg: 4, md: 2, sm: 1, xs: 1, xxs: 1 } as const;
@@ -26,32 +27,32 @@ function DashboardViewPage() {
 
   return (
     <Layout>
-      <PanelGroup direction="horizontal" className="w-full">
+      <PanelGroup direction='horizontal' className='w-full'>
         <Panel>
-          <div className="p-4">
-            <div className="flex justify-between items-center">
+          <div className='p-4'>
+            <div className='flex justify-between items-center'>
               <Link
                 to={"/dashboards"}
-                className="text-blue-500 hover:underline"
+                className='text-blue-500 hover:underline'
               >
                 &lt; Torna alla lista
               </Link>
             </div>
-            <div className="">
+            <div className=''>
               {isLoading && <Loading />}
               {error && (
-                <div role="alert" className="alert alert-error">
+                <div role='alert' className='alert alert-error'>
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 shrink-0 stroke-current"
-                    fill="none"
-                    viewBox="0 0 24 24"
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-6 w-6 shrink-0 stroke-current'
+                    fill='none'
+                    viewBox='0 0 24 24'
                   >
                     <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
                     />
                   </svg>
                   <span>{error.message}</span>
@@ -59,11 +60,11 @@ function DashboardViewPage() {
               )}
               {loaded && (
                 <div>
-                  <h1 className="text-4xl font-bold">{name}</h1>
-                  <h4 className="text-xl">{description}</h4>
-                  <div className="relative border min-h-[60vh]">
+                  <h1 className='text-4xl font-bold'>{name}</h1>
+                  <h4 className='text-xl'>{description}</h4>
+                  <div className='relative border min-h-[60vh] '>
                     <ResponsiveReactGridLayout
-                      className="react-grid-layout"
+                      className='react-grid-layout'
                       layouts={{
                         lg: layout,
                       }}
@@ -73,14 +74,27 @@ function DashboardViewPage() {
                     >
                       {layout.map((item) => (
                         <div
-                          className="react-grid-item overflow-hidden"
+                          className='react-grid-item overflow-hidden'
                           key={item.i}
                         >
-                          <div>
-                            <h3>
-                              <b>{charts[item.i].name}</b>
+                          <div className={`${ch}`}>
+                            <h3 className='bg-primary text-primary-content p-2'>
+                              <b>
+                                {WIDGET_HEIGHT} {charts[item.i].name}
+                              </b>
                             </h3>
-                            <p>{charts[item.i].description}</p>
+
+                            {/* <div role='tablist' className='btn tabs tabs-lift'>
+                              <a role='tab' className='tab'>
+                                description
+                              </a>
+                              <a role='tab' className='tab'>
+                                source
+                              </a>
+                              <a role='tab' className='tab'>
+                                info
+                              </a>
+                            </div> */}
                           </div>
                           {charts && charts[item.i] && (
                             <RenderChart
